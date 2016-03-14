88903be800b0cbdd20f2f4c324c087e9885af59f
Kyle E. Mitchell <kyle@kemitchell.com>
2016-03-14 13:47:52 -0700

    Export handler function generator

--- handler.js
+++ handler.js
@@ -1,11 +1,10 @@
-module.exports = handler
+module.exports = handlerGenerator
 
 var Busboy = require('busboy')
 var FormData = require('form-data')
 var bole = require('bole')
 var fs = require('fs')
 var https = require('https')
-var path = require('path')
 var peoplestring = require('peoplestring-parse')
 var url = require('url')
 var uuid = require('uuid')
@@ -21,128 +20,119 @@ var VERSION = require('./package.json').version
 
 var log = bole(NAME)
 
-var DOMAIN = process.env.DOMAIN
-
-var API_KEY = process.env.MAILGUN_API_KEY
-
-var POST_PATH = ( '/' + process.env.POST_PATH )
-
-var DISTRIBUTION_LIST =
-  ( process.env.DISTRIBUTION_LIST ||
-    path.join(process.cwd(), 'distribution_list') )
-
-function handler(request, response) {
-  request.log = log(uuid.v4())
-  request.log.info(request)
-  request.once('end', function() {
-    request.log.info(
-      { event: 'end',
-        status: response.statusCode }) })
-  var method = request.method
-  var parsedURL = url.parse(request.url)
-  if (parsedURL.pathname === POST_PATH) {
-    if (method === 'POST') {
-      readPostBody(request, function(error, fields) {
-        if (error) {
-          request.log.error(error)
-          response.statusCode = 500
-          response.end() }
-        else {
-          request.log.info(
-            { event: 'parsed fields',
-              fields: fields })
-          var from = peoplestring(fields.from)
-          readDistributionList(function(error, members) {
-            if (error) {
-              request.log.error(error)
-              request.statusCode = 500
-              request.end() }
-            else {
-              if (members.indexOf(from.email) < 0) {
-                request.log.info(
-                  { event: 'reject',
-                    from: from.email })
-                response.statusCode = 406
-                response.end() }
+function handlerGenerator(DOMAIN, API_KEY, POST_PATH, DISTRIBUTION_LIST) {
+  return function handler(request, response) {
+    request.log = log(uuid.v4())
+    request.log.info(request)
+    request.once('end', function() {
+      request.log.info(
+        { event: 'end',
+          status: response.statusCode }) })
+    var method = request.method
+    var parsedURL = url.parse(request.url)
+    if (parsedURL.pathname === POST_PATH) {
+      if (method === 'POST') {
+        readPostBody(request, function(error, fields) {
+          if (error) {
+            request.log.error(error)
+            response.statusCode = 500
+            response.end() }
+          else {
+            request.log.info(
+              { event: 'parsed fields',
+                fields: fields })
+            var from = peoplestring(fields.from)
+            readDistributionList(function(error, members) {
+              if (error) {
+                request.log.error(error)
+                request.statusCode = 500
+                request.end() }
               else {
-                var subject = fields.subject
-                var text = fields['stripped-text']
-                var otherMembers = members.filter(function(member) {
-                  return ( member !== from.email ) })
-                request.log.info({ event: 'distribute' })
-                distribute(otherMembers, subject, text, function(error) {
-                  if (error) {
-                    request.log.error(error)
-                    response.statusCode = 500
-                    response.end() }
-                  else {
-                    request.log.info({ event: 'sent' })
-                    response.statusCode = 200
-                    response.end() } }) } } }) } }) }
+                if (members.indexOf(from.email) < 0) {
+                  request.log.info(
+                    { event: 'reject',
+                      from: from.email })
+                  response.statusCode = 406
+                  response.end() }
+                else {
+                  var subject = fields.subject
+                  var text = fields['stripped-text']
+                  var otherMembers = members.filter(function(member) {
+                    return ( member !== from.email ) })
+                  request.log.info({ event: 'distribute' })
+                  distribute(otherMembers, subject, text, function(error) {
+                    if (error) {
+                      request.log.error(error)
+                      response.statusCode = 500
+                      response.end() }
+                    else {
+                      request.log.info({ event: 'sent' })
+                      response.statusCode = 200
+                      response.end() } }) } } }) } }) }
+      else {
+        request.statusCode = 405
+        request.end() } }
+    else if (parsedURL.pathname === '/') {
+      if (method === 'GET') {
+        response.end(( NAME + ' ' + VERSION + '\n' )) }
+      else {
+        response.statusCode = 415
+        response.end() } }
     else {
-      request.statusCode = 405
-      request.end() } }
-  else if (parsedURL.pathname === '/') {
-    if (method === 'GET') {
-      response.end(( NAME + ' ' + VERSION + '\n' )) }
-    else {
-      response.statusCode = 415
+      response.statusCode = 404
       response.end() } }
-  else {
-    response.statusCode = 404
-    response.end() } }
 
-function readPostBody(request, callback) {
-  var fields = { }
-  var busboy
-  try {
-    busboy = new Busboy({ headers: request.headers }) }
-  catch (error) {
-    callback(error)
-    return }
-  busboy.on('field', function(field, value) {
-    fields[field] = value })
-  busboy.once('finish', function() {
-    callback(null, fields) })
-  request.pipe(busboy) }
+  function readPostBody(request, callback) {
+    var fields = { }
+    var busboy
+    try {
+      busboy = new Busboy({ headers: request.headers }) }
+    catch (error) {
+      callback(error)
+      return }
+    busboy.on('field', function(field, value) {
+      fields[field] = value })
+    busboy.once('finish', function() {
+      callback(null, fields) })
+    request.pipe(busboy) }
 
-function readDistributionList(callback) {
-  fs.readFile(DISTRIBUTION_LIST, 'utf8', function(error, data) {
-    if (error) {
-      callback(error) }
-    else {
-      callback(null, data.toString().split('\n')) } }) }
+  function readDistributionList(callback) {
+    fs.readFile(DISTRIBUTION_LIST, 'utf8', function(error, data) {
+      if (error) {
+        callback(error) }
+      else {
+        callback(null, data.toString().split('\n')) } }) }
 
-function distribute(members, subject, text, callback) {
-  var form = new FormData()
-  form.append('from', ( 'list@' + DOMAIN ))
-  form.append('to', ( 'list@' + DOMAIN ))
-  form.append('bcc', members.join(','))
-  form.append('subject', subject)
-  form.append('text', text)
-  form.append('o:dkim', 'yes')
-  form.append('o:tracking', 'no')
-  form.append('o:tracking-clicks', 'no')
-  form.append('o:tracking-opens', 'no')
-  var options =
-    { method: 'POST',
-      host: 'api.mailgun.net',
-      path: ( '/v3/' + DOMAIN + '/messages' ),
-      auth: ( 'api:' + API_KEY ),
-      headers: form.getHeaders() }
-  var request = https.request(options)
-  request.once('response', function(response) {
-    var status = response.statusCode
-    if (status == 200) {
-      callback() }
-    else {
-      var buffers = [ ]
-      response
-        .on('data', function(buffer) {
-          buffers.push(buffer) })
-        .once('end', function() {
-          var body = Buffer.concat(buffers).toString()
-          var error = new Error(body)
-          error.statusCode = status
-          callback(error) }) } })
-  form.pipe(request) }
+  function distribute(members, subject, text, callback) {
+    var form = new FormData()
+    form.append('from', ( 'list@' + DOMAIN ))
+    form.append('to', ( 'list@' + DOMAIN ))
+    form.append('bcc', members.join(','))
+    form.append('subject', subject)
+    form.append('text', text)
+    form.append('o:dkim', 'yes')
+    form.append('o:tracking', 'no')
+    form.append('o:tracking-clicks', 'no')
+    form.append('o:tracking-opens', 'no')
+    var options =
+      { method: 'POST',
+        host: 'api.mailgun.net',
+        path: ( '/v3/' + DOMAIN + '/messages' ),
+        auth: ( 'api:' + API_KEY ),
+        headers: form.getHeaders() }
+    var request = https.request(options)
+    request.once('response', function(response) {
+      var status = response.statusCode
+      if (status == 200) {
+        callback() }
+      else {
+        var buffers = [ ]
+        response
+          .on('data', function(buffer) {
+            buffers.push(buffer) })
+          .once('end', function() {
+            var body = Buffer.concat(buffers).toString()
+            var error = new Error(body)
+            error.statusCode = status
+            callback(error) }) } })
+    form.pipe(request) } }
--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 #!/usr/bin/env node
 require('http')
-  .createServer(require('./handler'))
+  .createServer(
+    require('./handler')(
+      process.env.DOMAIN,
+      process.env.MAILGUN_API_KEY,
+      ( '/' + process.env.POST_PATH ),
+      ( process.env.DISTRIBUTION_LIST ||
+        require('path').join(process.cwd(), 'distribution_list') )) )
   .listen(( process.env.PORT || 8080 ), function() {
     process.stdout.write(
       ( 'Listening on port ' +
